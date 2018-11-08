f00947d68137afc541773fe44bee93d6d8fc7988
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-08 16:36:07 +0700

    Adjust Header positino

--- src/Header/HeaderStyledComponents.js
+++ src/Header/HeaderStyledComponents.js
@@ -1,23 +1,25 @@
 import styled from 'styled-components'
 
 export default styled.div`
-    width: 100%;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    padding: 24px;
+    padding: 42px;
+    margin-top: 12px;
     .info-wrapper {
         .text {
             color: #79797E;
             font-size: 36px;
             font-weight: bold;
-            margin-bottom: 12px;
+            margin-bottom: 24px;
+            opacity: 0.87;
         }
         .progress {
             color: #5C76E4;
             font-size: 18px;
             font-weight: bold;
+            opacity: 0.87;
         }
     }
 `
