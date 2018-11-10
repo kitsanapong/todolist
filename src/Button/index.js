d4c84a8f0bc2b167a7e1212f9469d6014a9587b2
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2018-11-10 13:59:57 +0700

    Add click event for Button

--- src/Button/index.js
+++ src/Button/index.js
@@ -5,13 +5,17 @@ import ButtonStyledComponents from './ButtonStyledComponents'
 class Button extends Component {
   render() {
     return (
-      <ButtonStyledComponents>
+      <ButtonStyledComponents onClick={this.props.onClick.bind(this)}>
         <div className="wrapper">
             <div className="content">+</div>
         </div>
       </ButtonStyledComponents>
     );
   }
+}
+
+Button.defaultProps = {
+  onClick: () => { console.log('Click') },
 }
 
 export default Button;
